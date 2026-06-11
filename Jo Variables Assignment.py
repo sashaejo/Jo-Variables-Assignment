a599e5380706aad191dca64d052ca8dcd2a1b781
sashaejo <sashaejo@gmail.com>
2026-06-11 12:07:20 -0400

    commit

---
+++ Jo Variables Assignment.py
@@ -0,0 +1,31 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+answer = num1 * num2
+print("The answer to your query is:",answer)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+answer = num1 / num2
+print("The answer to your query is:",answer)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+answer = num1 // num2
+print("The answer to your query is:",answer)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+answer = num1 % num2
+print("The answer to your query is:",answer)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+answer = num1 + num2
+print("The answer to your query is:",answer)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+answer = num1 - num2
+print("The answer to your query is:",answer)
+
+print("Sasha Jo")
